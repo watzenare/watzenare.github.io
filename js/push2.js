2628472745fb931de9b7bd36f54168c7eef30fcb
Eloi Watzenare <eloi.ballara@gmail.com>
2015-07-01 12:17:27 +0200

    Autopush

--- js/push2.js
+++ js/push2.js
@@ -29,35 +29,22 @@ function initialiseState() {
 
   // We need the service worker registration to check for a subscription
   navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-  console.log("ini6");
     // Do we already have a push message subscription?
-    serviceWorkerRegistration.pushManager.subscribe()
-      .then(function(subscription) {
-  console.log("ini7");
-        // Enable any UI which subscribes / unsubscribes from
-        // push messages.
-
+    serviceWorkerRegistration.pushManager.getSubscription().then(function(subscription) {
         if (!subscription) {
-  console.log("ini8");
           // We aren't subscribed to push, so set UI
           // to allow the user to enable push
           return;
         }
 
-  console.log("ini9");
         // Keep your server in sync with the latest subscriptionId
         sendSubscriptionToServer(subscription);
-
-        // Set your UI to show they have subscribed for
-        // push messages
       })
       .catch(function(err) {
-  console.log("ini10err");
         console.warn('Error during getSubscription()', err);
       });
   })
   .catch(function(err) {
-  console.log("ini11err");
     console.warn('Error during initialiseState()', err);
   });
 }
@@ -66,74 +53,74 @@ function sendSubscriptionToServer(subscription) {
   console.log(subscription.subscriptionId);
 }
 
-// function subscribe() {
-//   console.log("subs1");
-//   // Disable the button so it can't be changed while
-//   // we process the permission request
-//   navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-//     console.log("subs2");
-//     serviceWorkerRegistration.pushManager.subscribe()
-//       .then(function(subscription) {
-//         console.log("subs3");
-//         // The subscription was successful
+function subscribe() {
+  console.log("subs1");
+  // Disable the button so it can't be changed while
+  // we process the permission request
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    console.log("subs2");
+    serviceWorkerRegistration.pushManager.subscribe()
+      .then(function(subscription) {
+        console.log("subs3");
+        // The subscription was successful
 
-//         return sendSubscriptionToServer(subscription);
-//       })
-//       .catch(function(e) {
-//         console.log("subs4");
-//         if (Notification.permission === 'denied') {
-//           // The user denied the notification permission which
-//           // means we failed to subscribe and the user will need
-//           // to manually change the notification permission to
-//           // subscribe to push messages
-//           console.warn('Permission for Notifications was denied');
-//         } else {
-//           // A problem occurred with the subscription; common reasons
-//           // include network errors, and lacking gcm_sender_id and/or
-//           // gcm_user_visible_only in the manifest.
-//           console.error('Unable to subscribe to push.', e);
-//         }
-//       });
-//   }).catch(function(e) {
-//     console.log("subs5");
-//   console.error(e);
-// });
-// }
+        return sendSubscriptionToServer(subscription);
+      })
+      .catch(function(e) {
+        console.log("subs4");
+        if (Notification.permission === 'denied') {
+          // The user denied the notification permission which
+          // means we failed to subscribe and the user will need
+          // to manually change the notification permission to
+          // subscribe to push messages
+          console.warn('Permission for Notifications was denied');
+        } else {
+          // A problem occurred with the subscription; common reasons
+          // include network errors, and lacking gcm_sender_id and/or
+          // gcm_user_visible_only in the manifest.
+          console.error('Unable to subscribe to push.', e);
+        }
+      });
+  }).catch(function(e) {
+    console.log("subs5");
+  console.error(e);
+});
+}
 
 
 
-// function unsubscribe() {
+function unsubscribe() {
 
-//   navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-//     // To unsubscribe from push messaging, you need get the
-//     // subscription object, which you can call unsubscribe() on.
-//     serviceWorkerRegistration.pushManager.getSubscription().then(
-//       function(pushSubscription) {
-//         // Check we have a subscription to unsubscribe
-//         if (!pushSubscription) {
-//           // No subscription object, so set the state
-//           // to allow the user to subscribe to push
-//           return;
-//         }
+  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+    // To unsubscribe from push messaging, you need get the
+    // subscription object, which you can call unsubscribe() on.
+    serviceWorkerRegistration.pushManager.getSubscription().then(
+      function(pushSubscription) {
+        // Check we have a subscription to unsubscribe
+        if (!pushSubscription) {
+          // No subscription object, so set the state
+          // to allow the user to subscribe to push
+          return;
+        }
 
-//         var subscriptionId = pushSubscription.subscriptionId;
-//         return sendSubscriptionToServer(pushSubscription);
+        var subscriptionId = pushSubscription.subscriptionId;
+        return sendSubscriptionToServer(pushSubscription);
 
-//         // We have a subscription, so call unsubscribe on it
-//         pushSubscription.unsubscribe().then(function(successful) {
-//         }).catch(function(e) {
-//           // We failed to unsubscribe, this can lead to
-//           // an unusual state, so may be best to remove
-//           // the users data from your data store and
-//           // inform the user that you have done so
+        // We have a subscription, so call unsubscribe on it
+        pushSubscription.unsubscribe().then(function(successful) {
+        }).catch(function(e) {
+          // We failed to unsubscribe, this can lead to
+          // an unusual state, so may be best to remove
+          // the users data from your data store and
+          // inform the user that you have done so
 
-//           console.log('Unsubscription error: ', e);
-//         });
-//       }).catch(function(e) {
-//         console.error('Error thrown while unsubscribing from push messaging.', e);
-//       });
-//   });
-// }
+          console.log('Unsubscription error: ', e);
+        });
+      }).catch(function(e) {
+        console.error('Error thrown while unsubscribing from push messaging.', e);
+      });
+  });
+}
 
 // Opens an alert asking to the user if he wants to receive notifications
 Notification.requestPermission(function(result) {
