e6381ea448279c01af966f890d538df631b01cb4
Eloi Watzenare <eloi.ballara@gmail.com>
2015-07-01 17:00:45 +0200

    Autopush

--- js/push2.js
+++ js/push2.js
@@ -134,7 +134,7 @@ window.addEventListener('load', function() {
     navigator.serviceWorker.register('js/sw.js').then(initialiseState);
     subscribe();
   } else {
-    document.getElementById("perm").innerHTML = "Push is no available for your browser (use Chrome or Firefox updated)";
+    $("#perm").html("Push is no available for your browser (use Chrome or Firefox updated)");
     console.warn('Service workers aren\'t supported in this browser.');
   }
 });
