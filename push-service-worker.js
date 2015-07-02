9f09586d6803ef5a92543fe20ead6012d8588712
Eloi Watzenare <eloi.ballara@gmail.com>
2015-07-02 18:30:55 +0200

    Autopush

--- push-service-worker.js
+++ push-service-worker.js
@@ -35,9 +35,12 @@ self.addEventListener('push', function(event) {
 // If user click on the notification window, we have to take him to the interesting content
 self.addEventListener('notificationclick', function(event) {
     console.log('On notification click: ', event.notification.tag);
-    // // Android doesn't close the notification when you click on it
-    // // See: http://crbug.com/463146
-    // event.notification.close();
+    // Android doesn't close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
+
+    console.log(event);
+    console.log(event.notification);
 
     // // This looks to see if the current window is already open and focuses if it is
     // event.waitUntil(
