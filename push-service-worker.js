c81af695de11872d3978f870fbc05d0a1589b0ce
Eloi Watzenare <eloi.ballara@gmail.com>
2015-07-02 18:07:18 +0200

    Autopush

--- push-service-worker.js
+++ push-service-worker.js
@@ -1,31 +1,43 @@
 // This is the serviceworker that will be running on background when a push is received
 
 self.addEventListener('push', function(event) {
-
+console.log(event);
     var data = {};
     var icon = 'https://cdn3.iconfinder.com/data/icons/supermario/PNG/retro-mushroom-super-2.png';
 
-    if (event.data) {
-        data = event.data.json();
-        console.log('Received a push message', event);
-    } else {
-        console.log('No push data received', event);
-        return;
-    }
+    // if (event.data) {
+    //     data = event.data.json();
+    //     console.log('Received a push message', event);
+    // } else {
+    //     console.log('No push data received', event);
+    //     return;
+    // }
 
     // var title = 'New push!';
     // var body = 'You have received a push message.';
     // var tag = 'simple-push-demo-notification-tag';
 
+    // event.waitUntil(
+    //     self.registration.showNotification(data.title, {
+    //         body: data.body,
+    //         icon: icon,
+    //         tag: data.tag
+    //     })
+    // );
+    var title = 'Yay a message!';
+    var body = 'We have received a push message.';
+    var icon = 'https://cdn3.iconfinder.com/data/icons/supermario/PNG/retro-mushroom-super-2.png';
+    var tag = 'simple-push-demo-notification-tag';
+
     event.waitUntil(
-        self.registration.showNotification(data.title, {
-            body: data.body,
+        self.registration.showNotification(title, {
+            body: body,
             icon: icon,
-            tag: data.tag
+            tag: tag
         })
     );
 
-    // setTimeout(n.close.bind(event), 10000);
+    setTimeout(n.close.bind(event), 10000);
 });
 
 // // If user click on the notification window, we have to take him to the interesting content
